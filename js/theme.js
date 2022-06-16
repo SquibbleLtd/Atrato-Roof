0c3c81887df4756cf32ef97de6872732173dac36
Karl Cooper <karl@prolifik.co.uk>
2022-06-16 16:34:59 +0200

    Team members v1.1

--- js/theme.js
+++ js/theme.js
@@ -3,33 +3,35 @@ jQuery( document ).ready( function($) {
 
   $( '.att-link' ).click( function(e) {
     if ($( e.target ).is( '.att-linked-in a' ) ) {
-      return false;
-    }
+      // do nothing
+    } else {
 
-    if (!$( '#att-popup' ).length) {
-      $( 'body' ).append( '<div id="att-popup-backdrop" class="close-att-popup"></div><div id="att-popup"></div>' );
-    }
-
-    var dataarr = {
-      'action': 'atr_get_team_popup',
-      'team-member-id': $( this ).attr( 'data-team-id' )
-    };
-
-    $.ajax({
-      url: atr_ajax_object.ajax_url,
-      type: 'POST',
-      data: dataarr,
-      success: function(response){
-        $( '#loading' ).hide();
-        $( '#att-popup' ).html( response );
-        $( '#att-popup-backdrop, #att-popup' ).fadeIn();
-      },
-      error: function(xhr, status, error){
-        $( '#loading' ).hide();
-        var errorMessage = xhr.status + ': ' + xhr.statusText
-        alert( 'Error - ' + errorMessage );
+      if (!$( '#att-popup' ).length) {
+        $( 'body' ).append( '<div id="att-popup-backdrop" class="close-att-popup"></div><div id="att-popup"></div>' );
       }
-    });
+
+      var dataarr = {
+        'action': 'atr_get_team_popup',
+        'team-member-id': $( this ).attr( 'data-team-id' )
+      };
+
+      $.ajax({
+        url: atr_ajax_object.ajax_url,
+        type: 'POST',
+        data: dataarr,
+        success: function(response){
+          $( '#loading' ).hide();
+          $( '#att-popup' ).html( response );
+          $( '#att-popup-backdrop, #att-popup' ).fadeIn();
+        },
+        error: function(xhr, status, error){
+          $( '#loading' ).hide();
+          var errorMessage = xhr.status + ': ' + xhr.statusText
+          alert( 'Error - ' + errorMessage );
+        }
+      });
+
+    }
   });
 
   $( 'body' ).on( 'click', '.close-att-popup' , function() {
